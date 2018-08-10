a1bc0be97bb900cade9da2e3e0ce4a83259580fd
Weston Watson <westonrwatson@users.noreply.github.com>
2018-08-09 17:05:50 -0700

    Revert "0.0.1"
    This reverts commit 402bdba1828df243cfe1e32076198e7f8f073597.

--- js/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  // Mobile nav Functionality
-  $('#hamburger,#close').click(function() {
-    $('#hamburger,#close').toggle();
-    $('.c-navigation #links').toggleClass('visible');
-  });
-
-});
